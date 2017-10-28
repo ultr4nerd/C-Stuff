f692800736ac985672615038fe3c8063b657cfbe
Mauricio Chávez <mauriciochavez@ciencias.unam.mx>
2017-10-27 23:57:23 -0500

    First commit

---
+++ FactorialRecursivo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+long factorial(int n){
+    if (n >= 1)
+        return n*factorial(n-1);
+    else
+        return 1;
+}
+
+int main()
+{
+    int n;
+    printf("\nIngresa un entero positivo: ");
+    scanf("%d", &n);
+    printf("El factorial de %d es igual a: %ld \n\n", n, factorial(n));
+    return 0;
+}
+
